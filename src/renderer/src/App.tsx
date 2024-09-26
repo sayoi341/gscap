000c63fb9807f4ff4d8b01a38215a585d89ed9a1
Yusuke <67366917+Yusuke4869@users.noreply.github.com>
2024-09-26 23:56:30 +0900

    chore(biome): fix config and errors (#20)
    * chore(biome): add ignore files
    
    * chore(biome): fix commands
    
    * chore(biome): update config
    
    * fix: fix linter errors and format
    
    * ci: fix biome workflow

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,34 +2,34 @@ import electronLogo from "./assets/electron.svg";
 import Versions from "./components/Versions";
 
 function App(): JSX.Element {
-	const ipcHandle = (): void => window.electron.ipcRenderer.send("ping");
+  const ipcHandle = (): void => window.electron.ipcRenderer.send("ping");
 
-	return (
-		<>
-			<img alt="logo" className="logo" src={electronLogo} />
-			<div className="creator">Powered by electron-vite</div>
-			<div className="text">
-				Build an Electron app with <span className="react">React</span>
-				&nbsp;and <span className="ts">TypeScript</span>
-			</div>
-			<p className="tip">
-				Please try pressing <code>F12</code> to open the devTool
-			</p>
-			<div className="actions">
-				<div className="action">
-					<button type="button" rel="https://electron-vite.org/">
-						Documentation
-					</button>
-				</div>
-				<div className="action">
-					<button type="button" onClick={ipcHandle}>
-						Send IPC
-					</button>
-				</div>
-			</div>
-			<Versions />
-		</>
-	);
+  return (
+    <>
+      <img alt="logo" className="logo" src={electronLogo} />
+      <div className="creator">Powered by electron-vite</div>
+      <div className="text">
+        Build an Electron app with <span className="react">React</span>
+        &nbsp;and <span className="ts">TypeScript</span>
+      </div>
+      <p className="tip">
+        Please try pressing <code>F12</code> to open the devTool
+      </p>
+      <div className="actions">
+        <div className="action">
+          <button type="button" rel="https://electron-vite.org/">
+            Documentation
+          </button>
+        </div>
+        <div className="action">
+          <button type="button" onClick={ipcHandle}>
+            Send IPC
+          </button>
+        </div>
+      </div>
+      <Versions />
+    </>
+  );
 }
 
 export default App;
