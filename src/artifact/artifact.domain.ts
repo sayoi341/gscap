daca0f444a030b0b076575c825cac6b9388ea2c6
sayoi341 <me@sayoi341.moe>
2024-09-27 02:05:09 +0000

    fix: 🩹 聖遺物の誤字脱字、フォーマットの修正

--- src/artifact/artifact.domain.ts
+++ src/artifact/artifact.domain.ts
@@ -21,15 +21,15 @@ type ArtifactSet =
   | "prayersForSpringtime"
   | "gladiatorsFinale" // 剣闘士のフィナーレ
   | "wandersTroupe" // 大地を流浪する楽団
-  | "noblesseOblige" //旧貴族のしつけ
+  | "noblesseOblige" // 旧貴族のしつけ
   | "bloodstainedChivalry" // 血染めの騎士道
   | "maidenBeloved" // 愛される少女
   | "viridescentVenerer" // 翠緑の影
   | "archaicPetra" // 悠久の盤岩
   | "retracingBolide" // 逆飛びの流星
-  | "thunderSoother" // 雷を抑える心
-  | "lavawalker" // 火渡り
-  | "crimsonWitchOfFlames" // 炎魔女
+  | "thunderSoother" // 雷を鎮める尊者
+  | "lavawalker" // 烈火を渡る賢者
+  | "crimsonWitchOfFlames" // 燃え盛る炎の魔女
   | "blizzardStrayer" // 氷風を彷徨う勇士
   | "heartOfDepth" // 沈淪の心
   | "tenacityOfTheMillelith" // 千岩牢固
@@ -37,19 +37,19 @@ type ArtifactSet =
   | "shimenawasReminiscence" // 追憶のしめ縄
   | "emblemOfSeveredFate" // 絶縁の旗印
   | "huskOfOpulentDreams" // 華館夢醒形骸記
-  | "oceanHuedClam" // 海染しゃこ
-  | "vermillionHereafter" //辰砂往生録
-  | "echoesOfAnOffering" //来歆の余響
-  | "deepWoodMemories" // 森林の記憶
+  | "oceanHuedClam" // 海染硨磲
+  | "vermillionHereafter" // 辰砂往生録
+  | "echoesOfAnOffering" // 来歆の余響
+  | "deepWoodMemories" // 深林の記憶
   | "gildedDreams" // 金メッキの夢
-  | "desertPavilion" // 砂上の楼閣
+  | "desertPavilion" // 砂上の楼閣の史話
   | "flowerOfParadiseLost" // 楽園の絶花
   | "nymphsDream" // 水仙の夢
   | "vourukashasGlow" // 花海甘露の光
   | "MarechausseeHunter" // ファントムハンター
   | "goldenTroupe" // 黄金の劇団
-  | "songOfDaysPast" //在りし日の歌
-  | "nightTimeWhisperInTheEchoingWoods" // 残響の森で囁かれる夜話
+  | "songOfDaysPast" // 在りし日の歌
+  | "nighttimeWhisperInTheEchoingWoods" // 残響の森で囁かれる夜話
   | "fragmentOfHarmonicWhimsy" // 諧律奇想の断章
   | "unfinishedReverie" // 遂げられなかった想い
   | "scrollOfTheHeroOfCinderCity" // 灰燼の都に立つ英雄の絵巻
@@ -72,23 +72,23 @@ const artifactMainStatusMap = {
     "defPersent",
     "hpPersent",
     "elementalMastery", // 元素熟知
-    "physicalDmgBonus", //物理ダメージ
+    "physicalDmgBonus", // 物理ダメージ
     "pyroDmgBonus", // 炎ダメージバフ
-    "hydroDmgBonus", //水ダメージバフ
+    "hydroDmgBonus", // 水ダメージバフ
     "dendroDmgBonus", // 草ダメージバフ
-    "electroDmgBonus", //雷ダメージバフ
-    "anemoDmgBonus", //風ダメージバフ
-    "cryoDmgBonus", //　氷ダメージバフ
-    "geoDmgBonus",
-  ], //岩ダメージバフ
+    "electroDmgBonus", // 雷ダメージバフ
+    "anemoDmgBonus", // 風ダメージバフ
+    "cryoDmgBonus", // 氷ダメージバフ
+    "geoDmgBonus", //岩ダメージバフ
+  ],
   circlet: [
     "atkPersent",
     "defPersent",
     "hpPersent",
     "elementalMastery", // 元素熟知
-    "critRate", //会心率
-    "critDmg",
-  ], //会心ダメージ
+    "critRate", // 会心率
+    "critDmg", //会心ダメージ
+  ],
 } as const;
 
 type ArtifactMainStatus<T extends ArtifactPosition> =
