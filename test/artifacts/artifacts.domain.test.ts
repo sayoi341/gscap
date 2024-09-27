4b50cd5f3a91886652a210423493e06700ebb72e
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2024-09-27 16:01:32 +0900

    feat: implement artifacts (#21)
    * feat: ✨ artifactのdomainを作成  (#13)
    
    * test: 🧪 artifactのテストを作成  (#13)
    
    * fix: ジェネリクスの削除
    
    * format
    
    * feat: ✨ artifactのrepository interfaceの作成  (#13)
    
    * WIP: 🚧 artifactのuse caseの作成  (#13)
    
    * fix: 🩹 聖遺物の誤字脱字、フォーマットの修正
    
    * fix: 🩹 format
    
    * fix: 聖遺物の名前の修正
    
    * fix: artifactを複数形に変更

---
+++ test/artifacts/artifacts.domain.test.ts
@@ -0,0 +1,80 @@
+import { describe, expect, it } from "vitest";
+import { Artifacts } from "../../src/artifacts/artifacts.domain";
+
+describe("Artifact", () => {
+  it("インスタンス作成", () => {
+    const artifact = new Artifacts(
+      5,
+      "gladiatorsFinale",
+      "sands",
+      "atkPersent",
+      {
+        energyRecharge: 10,
+        critRate: 10,
+        critDmg: 10,
+      },
+    );
+
+    expect(artifact.stars).toBe(5);
+    expect(artifact.set).toBe("gladiatorsFinale");
+    expect(artifact.position).toBe("sands");
+    expect(artifact.mainStatus).toBe("atkPersent");
+    expect(artifact.subStatus).toEqual({
+      energyRecharge: 10,
+      critRate: 10,
+      critDmg: 10,
+    });
+  });
+
+  it("ハッシュの確認", () => {
+    const artifact = new Artifacts(
+      5,
+      "gladiatorsFinale",
+      "sands",
+      "atkPersent",
+      {
+        energyRecharge: 10,
+        critRate: 10,
+        critDmg: 10,
+      },
+    );
+
+    expect(artifact.hash).toBe(
+      "733be50f055a8a2f5a831c3048b0f0a6fda15f2f65c7580d8af298bc9aa897c3",
+    );
+  });
+
+  it("部位とメインステータスの不一致", () => {
+    expect(() => {
+      new Artifacts(5, "gladiatorsFinale", "sands", "hp", {
+        energyRecharge: 10,
+        critRate: 10,
+        critDmg: 10,
+        hp: 10,
+      });
+    }).toThrow("invalid artifact main status");
+  });
+
+  it("サブステータスが5つ以上", () => {
+    expect(() => {
+      new Artifacts(5, "gladiatorsFinale", "sands", "atkPersent", {
+        energyRecharge: 10,
+        critRate: 10,
+        critDmg: 10,
+        hp: 10,
+        atk: 10,
+      });
+    }).toThrow("artifact sub status should be less than 4");
+  });
+
+  it("サブステータスがメインステータスと同じ", () => {
+    expect(() => {
+      new Artifacts(5, "gladiatorsFinale", "sands", "atkPersent", {
+        energyRecharge: 10,
+        critRate: 10,
+        critDmg: 10,
+        atkPersent: 10,
+      });
+    }).toThrow("artifact sub status should not same main status");
+  });
+});
