904347bffdb9215255a76a41d38a5771d213014a
sayoi341 <me@sayoi341.moe>
2024-11-17 00:39:06 +0900

    bugfix: 🐛 viteのpathsの修正  (#40)

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,32 @@
-import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    build: {
+      lib: {
+        entry: "src/main/index.mts",
+        formats: ["cjs"],
+      },
+    },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        "@renderer": resolve("src/renderer/src"),
+    build: {
+      lib: {
+        entry: "src/preload/index.mts",
+        formats: ["cjs"],
       },
     },
-    plugins: [react()],
+  },
+  renderer: {
+    plugins: [react(), tsconfigPaths()],
+    build: {
+      rollupOptions: {
+        input: "src/renderer/index.html",
+      },
+    },
   },
 });
